3c8229c41bb42a2bfd73fff582c3f8831056227e
Sakthisowmi <sakthibca123@gmail.com>
2026-08-08 08:49:33 +0530

    build file changes

--- PlexoRepPortal/Program.cs
+++ PlexoRepPortal/Program.cs
@@ -33,15 +33,12 @@ namespace PlexoRepPortal
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            app.MapOpenApi();
+            app.UseSwaggerUI(options =>
             {
-                app.MapOpenApi();
-                app.UseSwaggerUI(options =>
-                {
-                    options.SwaggerEndpoint("/openapi/v1.json", "api");
+                options.SwaggerEndpoint("/openapi/v1.json", "api");
 
-                });
-            }
+            });
             app.UseCors("AllowOrigin");
             app.UseHttpsRedirection();
 
